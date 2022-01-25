bc59ef6eb7fd5b9f692a374bdfcc24e613764174
Evgeny Efimenko <evgeny.efimenko@gmail.com>
2022-01-25 18:26:30 +0300

    Performed some clean-up of create_splits.py

--- Project_1/create_splits.py
+++ Project_1/create_splits.py
@@ -17,39 +17,21 @@ def split(data_dir):
         - data_dir [str]: data directory, /home/workspace/data/waymo
     """
     
-    # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
-    # You should move the files rather than copy because of space limitations in the workspace.
     val_segments = []
     with open('validation.txt') as f:
         for line in f:
             val_segments.append(line.strip())
-    #print(val_segments)
-    file_list = glob.glob(os.path.join(data_dir, 'training_and_validation', '*.tfrecord'))
-    #print(file_list)
     
-    # Remove old files (actually old symbolic links) from validation set
-    #files = glob.glob(os.path.join(data_dir, 'val', '*.tfrecord'))
-    #for f in files:
-    #    os.remove(f) 
-        
-    # Remove old files (actually old symbolic links) from train set
-    #files = glob.glob(os.path.join(data_dir, 'train', '*.tfrecord'))
-    #for f in files:
-    #    os.remove(f)
-  
+    file_list = glob.glob(os.path.join(data_dir, 'training_and_validation', '*.tfrecord'))
+      
     for file in file_list:
         start = file.find('segment')
         end = file.find('_with_camera_labels')
         
         if file[start:end] in val_segments:
-            #print('File', file, ' moved to validation set')
-            #print(file, os.path.join(data_dir, 'val', file[start:]))
             os.rename(file, os.path.join(data_dir, 'val', file[start:]))
-            #os.symlink(file, os.path.join(data_dir, 'val', file[start:]))
         else:
-            #print('File', file, ' moved to training set')
             os.rename(file, os.path.join(data_dir, 'train', file[start:]))
-            #os.symlink(file, os.path.join(data_dir, 'train', file[start:]))
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
